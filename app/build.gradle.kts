eb31823702f7dcb426e141ffb836b7f381d77ae3
Prayag <prayag@PrayagtekiMac-mini.local>
2025-05-14 11:28:47 +0545

    upto date versions for aar publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.prayag"
             artifactId = "omr-scanner"
-            version = "0.0.1"
+            version = "0.1.4"
 
             afterEvaluate {
                 from(components["release"])
