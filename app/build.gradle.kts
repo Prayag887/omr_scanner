bc66c1ba9b0b44e3062dfdf14ebe5bb275979685
Prayag <prayag@PrayagtekiMac-mini.local>
2025-05-18 11:06:36 +0545

    shrink resource not needed for aar file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
+//            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
