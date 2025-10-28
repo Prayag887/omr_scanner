97161cf206fd13040b637ef7616c06a12fa2f3ec
Prayag887 <prayagdhakal123@gmail.com>
2025-10-28 14:54:15 +0545

    application build instead of aar, version upgrade, blur issues being fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,25 +128,25 @@ dependencies {
     implementation(project(":OpenCV-sdk"))
 
     // Latest ML Kit Document Scanner dependency
-//    implementation ("com.google.android.gms:play-services-mlkit-document-scanner:16.0.0-beta1")
+//    implementation ("com.google.android.gms:play-services-mlkit-document-scanner:16.0.0")
 
     // For ML Kit base functionality
-    implementation ("com.google.mlkit:vision-common:17.3.0")
+//    implementation ("com.google.mlkit:vision-common:17.3.0")
 
     // CameraX Core Library
-    implementation ("androidx.camera:camera-core:1.4.2")
+    implementation ("androidx.camera:camera-core:1.5.1")
 
     // CameraX Camera2 Implementation
-    implementation ("androidx.camera:camera-camera2:1.4.2")
+    implementation ("androidx.camera:camera-camera2:1.5.1")
 
     // CameraX Lifecycle
     implementation ("androidx.camera:camera-lifecycle:1.4.2")
 
     // CameraX View for Preview
-    implementation ("androidx.camera:camera-view:1.4.2")
+    implementation ("androidx.camera:camera-view:1.5.1")
 
     // Optional: For image analysis
-    implementation ("androidx.camera:camera-extensions:1.4.2")
+    implementation ("androidx.camera:camera-extensions:1.5.1")
 
 
     implementation(libs.document.scanner)
